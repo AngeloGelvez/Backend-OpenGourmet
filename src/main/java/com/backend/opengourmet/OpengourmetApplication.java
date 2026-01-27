cd55950a95670cbb195f02a816ab520354e32293
AngeloGelvez <angelogelvez14@gmail.com>
2026-01-27 15:40:35 -0500

    Backend-v1

---
+++ src/main/java/com/backend/opengourmet/OpengourmetApplication.java
@@ -0,0 +1,13 @@
+package com.backend.opengourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpengourmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpengourmetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/opengourmet/OpengourmetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.opengourmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpengourmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
